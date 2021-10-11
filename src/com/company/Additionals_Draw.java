aae2bda587dd3c78435e9ca8b4ff47d96aa76eb1
Дмитрий Александров <mapoch2002@yandex.ru>
2021-10-11 15:49:00 +0400

    дополнения в ходе урока

--- src/com/company/Additionals_Draw.java
+++ src/com/company/Additionals_Draw.java
@@ -3,5 +3,5 @@ import java.awt.*;
 
 public interface Additionals_Draw {
     void setAmount(int k);
-    void Draw(Graphics2D g, Color color, boolean Back_state, boolean Bombs_state, int X, int Y);
+    void Draw(Graphics2D g, Color color, int X, int Y);
 }
--- src/com/company/Outer_rockets.java
+++ src/com/company/Outer_rockets.java
@@ -22,79 +22,72 @@ public class Outer_rockets implements Additionals_Draw{
         return this.Amount;
     }
 
-    public void Draw(Graphics2D g, Color color, boolean Back_state, boolean Bombs_state, int X, int Y) {
-
+    public void Draw(Graphics2D g, Color color, int X, int Y) {
         g.setColor(color);
 
-        if (Back_state) {
-            g.drawOval(X + 90, Y + 30, 10, 10);
-            g.drawLine(X + 100, Y + 35, X + 105, Y + 35);
-        }
 
-        if (Bombs_state) {
-            Polygon rocket;
-            X += 40;
-            switch (Amount) {
-                case Two:
-                    Y += 15;
-                    for (int i = 0; i < 2; i++) {
-                        rocket = new Polygon();
-                        rocket.addPoint(X - 5, Y + 5);
-                        rocket.addPoint(X, Y + 3);
-                        rocket.addPoint(X + 12, Y + 3);
-                        rocket.addPoint(X + 14, Y + 1);
-                        rocket.addPoint(X + 20, Y + 1);
-                        rocket.addPoint(X + 20, Y + 9);
-                        rocket.addPoint(X + 14, Y + 9);
-                        rocket.addPoint(X + 12, Y + 7);
-                        rocket.addPoint(X, Y + 7);
+        Polygon rocket;
+        X += 40;
+        switch (Amount) {
+            case Two:
+                Y += 15;
+                for (int i = 0; i < 2; i++) {
+                    rocket = new Polygon();
+                    rocket.addPoint(X - 5, Y + 5);
+                    rocket.addPoint(X, Y + 3);
+                    rocket.addPoint(X + 12, Y + 3);
+                    rocket.addPoint(X + 14, Y + 1);
+                    rocket.addPoint(X + 20, Y + 1);
+                    rocket.addPoint(X + 20, Y + 9);
+                    rocket.addPoint(X + 14, Y + 9);
+                    rocket.addPoint(X + 12, Y + 7);
+                    rocket.addPoint(X, Y + 7);
 
-                        g.fillPolygon(rocket);
+                    g.fillPolygon(rocket);
 
-                        Y += 30;
-                    }
-                    break;
-                case Four:
-                    Y += 10;
-                    for (int i = 0; i < 4; i++) {
-                        rocket = new Polygon();
-                        rocket.addPoint(X - 3, Y + 3);
-                        rocket.addPoint(X, Y + 2);
-                        rocket.addPoint(X + 12, Y + 2);
-                        rocket.addPoint(X + 14, Y);
-                        rocket.addPoint(X + 18, Y);
-                        rocket.addPoint(X + 18, Y + 6);
-                        rocket.addPoint(X + 14, Y + 6);
-                        rocket.addPoint(X + 12, Y + 4);
-                        rocket.addPoint(X, Y + 4);
+                    Y += 30;
+                }
+                break;
+            case Four:
+                Y += 10;
+                for (int i = 0; i < 4; i++) {
+                    rocket = new Polygon();
+                    rocket.addPoint(X - 3, Y + 3);
+                    rocket.addPoint(X, Y + 2);
+                    rocket.addPoint(X + 12, Y + 2);
+                    rocket.addPoint(X + 14, Y);
+                    rocket.addPoint(X + 18, Y);
+                    rocket.addPoint(X + 18, Y + 6);
+                    rocket.addPoint(X + 14, Y + 6);
+                    rocket.addPoint(X + 12, Y + 4);
+                    rocket.addPoint(X, Y + 4);
 
-                        g.fillPolygon(rocket);
+                    g.fillPolygon(rocket);
 
-                        if (i == 1) Y += 25;
-                        else Y += 10;
-                    }
-                    break;
-                case Six:
-                    Y += 10;
-                    for (int i = 0; i < 6; i++) {
-                        rocket = new Polygon();
-                        rocket.addPoint(X - 3, Y + 2);
-                        rocket.addPoint(X, Y + 1);
-                        rocket.addPoint(X + 12, Y + 1);
-                        rocket.addPoint(X + 14, Y);
-                        rocket.addPoint(X + 18, Y);
-                        rocket.addPoint(X + 18, Y + 4);
-                        rocket.addPoint(X + 14, Y + 4);
-                        rocket.addPoint(X + 12, Y + 3);
-                        rocket.addPoint(X, Y + 3);
+                    if (i == 1) Y += 25;
+                    else Y += 10;
+                }
+                break;
+            case Six:
+                Y += 10;
+                for (int i = 0; i < 6; i++) {
+                    rocket = new Polygon();
+                    rocket.addPoint(X - 3, Y + 2);
+                    rocket.addPoint(X, Y + 1);
+                    rocket.addPoint(X + 12, Y + 1);
+                    rocket.addPoint(X + 14, Y);
+                    rocket.addPoint(X + 18, Y);
+                    rocket.addPoint(X + 18, Y + 4);
+                    rocket.addPoint(X + 14, Y + 4);
+                    rocket.addPoint(X + 12, Y + 3);
+                    rocket.addPoint(X, Y + 3);
 
-                        g.fillPolygon(rocket);
+                    g.fillPolygon(rocket);
 
-                        if (i == 2) Y += 20;
-                        else Y += 7;
-                    }
-                    break;
-            }
+                    if (i == 2) Y += 20;
+                    else Y += 7;
+                }
+                break;
         }
     }
 }
