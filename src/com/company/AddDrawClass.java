e5e0d2d8d0b56c934e855973000b2121ff122d7e
Дмитрий Александров <mapoch2002@yandex.ru>
2021-10-11 15:41:48 +0400

    дополнения в ходе урока

--- src/com/company/AddDrawClass.java
+++ src/com/company/AddDrawClass.java
@@ -25,76 +25,67 @@ public class AddDrawClass {
         return this.Amount;
     }
 
-    public void Draw(Graphics2D g, Color color, boolean Back_state, boolean Bombs_state, int X, int Y) {
+    public void Draw(Graphics2D g, Color color, int X, int Y) {
 
         g.setColor(color);
 
-        if (Back_state == true)
+        Polygon[] bombs_plumages;
+        X += 20;
+        Y += 31;
+        switch (Amount)
         {
-            g.drawOval(X + 90, Y + 30, 10, 10);
-            g.drawLine(X + 100, Y + 35,X + 105, Y + 35);
-        }
+            case Two:
+                bombs_plumages = new Polygon[2];
 
-        if (Bombs_state == true)
-        {
-            Polygon[] bombs_plumages;
-            X += 20;
-            Y += 31;
-            switch (Amount)
-            {
-                case Two:
-                    bombs_plumages = new Polygon[2];
+                for (int i = 0; i < 2; i++) {
+                    bombs_plumages[i] = new Polygon();
+                    bombs_plumages[i].addPoint(X + 20, Y + 4);
+                    bombs_plumages[i].addPoint(X + 25, Y);
+                    bombs_plumages[i].addPoint(X + 25, Y + 8);
 
-                    for (int i = 0; i < 2; i++) {
-                        bombs_plumages[i] = new Polygon();
-                        bombs_plumages[i].addPoint(X + 20, Y + 4);
-                        bombs_plumages[i].addPoint(X + 25, Y);
-                        bombs_plumages[i].addPoint(X + 25, Y + 8);
+                    g.fillOval(X, Y, 20, 8);
 
-                        g.fillOval(X, Y, 20, 8);
+                    g.fillPolygon(bombs_plumages[i]);
 
-                        g.fillPolygon(bombs_plumages[i]);
+                    X += 30;
+                }
+                break;
+            case Four:
+                bombs_plumages = new Polygon[4];
+                X -= 3;
+                Y++;
 
-                        X += 30;
-                    }
-                    break;
-                case Four:
-                    bombs_plumages = new Polygon[4];
-                    X -= 3;
-                    Y++;
+                for (int i = 0; i < 4; i++) {
+                    bombs_plumages[i] = new Polygon();
+                    bombs_plumages[i].addPoint(X + 10, Y + 3);
+                    bombs_plumages[i].addPoint(X + 15, Y - 2);
+                    bombs_plumages[i].addPoint(X + 15, Y + 8);
 
-                    for (int i = 0; i < 4; i++) {
-                        bombs_plumages[i] = new Polygon();
-                        bombs_plumages[i].addPoint(X + 10, Y + 3);
-                        bombs_plumages[i].addPoint(X + 15, Y - 2);
-                        bombs_plumages[i].addPoint(X + 15, Y + 8);
+                    g.fillOval(X, Y, 10, 6);
 
-                        g.fillOval(X, Y, 10, 6);
+                    g.fillPolygon(bombs_plumages[i]);
 
-                        g.fillPolygon(bombs_plumages[i]);
+                    X += 18;
+                }
+                break;
+            case Six:
+                bombs_plumages = new Polygon[6];
+                X -= 3;
+                Y++;
 
-                        X += 18;
-                    }
-                    break;
-                case Six:
-                    bombs_plumages = new Polygon[6];
-                    X -= 3;
-                    Y++;
+                for (int i = 0; i < 6; i++) {
+                    bombs_plumages[i] = new Polygon();
+                    bombs_plumages[i].addPoint(X + 5, Y + 3);
+                    bombs_plumages[i].addPoint(X + 10, Y - 1);
+                    bombs_plumages[i].addPoint(X + 10, Y + 7);
 
-                    for (int i = 0; i < 6; i++) {
-                        bombs_plumages[i] = new Polygon();
-                        bombs_plumages[i].addPoint(X + 5, Y + 3);
-                        bombs_plumages[i].addPoint(X + 10, Y - 1);
-                        bombs_plumages[i].addPoint(X + 10, Y + 7);
+                    g.fillOval(X, Y, 5, 6);
 
-                        g.fillOval(X, Y, 5, 6);
+                    g.fillPolygon(bombs_plumages[i]);
 
-                        g.fillPolygon(bombs_plumages[i]);
-
-                        X += 12;
-                    }
-                    break;
-            }
+                    X += 12;
+                }
+                break;
         }
     }
 }
